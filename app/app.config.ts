ac1051562b683112f4926078b150983940624c03
José Lazaretti <eduardo1793@gmail.com>
2026-01-13 13:56:48 -0300

    Primary orange

--- app/app.config.ts
+++ app/app.config.ts
@@ -1,8 +1,9 @@
 export default defineAppConfig({
   ui: {
     colors: {
-      primary: 'purple',
+      primary: 'orange',
       secondary: 'green',
+      neutral: 'no-color',
     },
   },
 });
