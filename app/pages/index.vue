364f7e17949c579e4c0228ae663e5eebf98a3836
José Lazaretti <eduardo1793@gmail.com>
2026-01-13 15:12:37 -0300

    Add FAQ

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -72,5 +72,15 @@
         <ButtonAgendarConsulta />
       </div>
     </section>
+
+    <div class="border-b-8 border-b-yellow-100 dark:border-b-yellow-200"></div>
+
+    <section class="pt-20 pb-20 text-center bg-gray-700 dark:bg-gray-800 text-white">
+      <div
+        class="container max-w-4xl justify-self-center flex flex-col items-center justify-center"
+      >
+        <Faq />
+      </div>
+    </section>
   </div>
 </template>
