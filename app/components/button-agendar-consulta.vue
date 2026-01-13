c2e69423b290e5250159dc2ca650bfcee9452f7c
José Lazaretti <eduardo1793@gmail.com>
2026-01-13 13:57:10 -0300

    Button agendar

--- app/components/button-agendar-consulta.vue
+++ app/components/button-agendar-consulta.vue
@@ -2,14 +2,9 @@
   <UButton
     class="pt-5 pb-5 pl-10 pr-10"
     label="AGENDAR CONSULTA AGORA"
-    color="neutral"
-    variant="soft"
-    @click="agendarConsulta"
+    color="primary"
+    variant="solid"
+    to="https://wa.me/+554491075497"
+    target="_blank"
   />
 </template>
-
-<script setup lang="ts">
-const agendarConsulta = () => {
-  window.open('https://calendly.com/tuane-correia/30min', '_blank');
-};
-</script>
