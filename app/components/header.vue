3c739801a6271bdafc70bef6956cd5872a42c3be
José Lazaretti <eduardo1793@gmail.com>
2026-09-10 19:09:17 -0300

    update header

--- app/components/header.vue
+++ app/components/header.vue
@@ -1,12 +1,10 @@
 <template>
-  <header class="sticky top-0 z-50 bg-white dark:bg-gray-900 py-3 sm:py-5 shadow-sm">
+  <header class="sticky top-0 z-50 bg-brand-dark-green dark:bg-gray-900 py-3 sm:py-5 shadow-sm">
     <div class="container max-w-4xl mx-auto px-4">
       <div class="flex justify-between items-center gap-2">
         <NuxtLink to="/" class="min-w-0 text-left text-gray-900 dark:text-gray-200">
-          <div class="text-base xs:text-xl sm:text-3xl font-semibold truncate">
-            Tuane Marcela Correia
-          </div>
-          <div class="text-xs sm:text-lg font-thin">Nutricionista</div>
+          <div class="text-base xs:text-xl sm:text-3xl font-semibold truncate">Tuane Correia</div>
+          <div class="text-xs sm:text-lg font-thin">NUTRICIONISTA</div>
         </NuxtLink>
         <div class="flex items-center gap-1 sm:gap-2 shrink-0">
           <ButtonAcessarSistema />
