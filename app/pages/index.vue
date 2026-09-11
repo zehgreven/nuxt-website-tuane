e6bd755ebd26d3a133be7f87516e817f80854c71
José Lazaretti <eduardo1793@gmail.com>
2026-09-11 19:53:09 -0300

    remove old code

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -79,54 +79,6 @@
       </div>
     </section>
 
-    <section class="py-20 px-5 text-center bg-gray-700 dark:bg-gray-800 text-white">
-      <div
-        class="container max-w-4xl justify-self-center flex flex-col items-center justify-center"
-      >
-        <div class="text-4xl mb-10">Se você…</div>
-
-        <div class="grid grid-cols-1 md:grid-cols-2 gap-5 mb-10">
-          <HBox class="col-span-1" icon="pixel:health-stool">
-            Não consegue parar de alternar entre diarreia e constipação
-          </HBox>
-          <HBox class="col-span-1" icon="pixel:design-ruler">
-            Não consegue diminuir o estufamento na barriga
-          </HBox>
-          <HBox class="col-span-1" icon="pixel:healthy-food-dish">
-            Está cansada de se sentir constrangida por causa dos sintomas da SII
-          </HBox>
-          <HBox class="col-span-1" icon="pixel:health-medical-notes">
-            Sente que já tentou de tudo para lidar com a Síndrome do Intestino Irritável
-          </HBox>
-        </div>
-
-        <div class="text-2xl mb-5">
-          Saiba, <strong>você pode controlar</strong> de uma vez por todas os
-          <strong>seus sintomas</strong>
-        </div>
-        <div class="text-lg mb-5">
-          Porém, <strong>não conseguirá atingir</strong> esse resultado
-          <strong>se continuar fazendo</strong> uso de <strong>dietas generalistas</strong>
-        </div>
-        <div class="text-lg mb-5">
-          <strong>Você já deve ter pesquisado</strong> inúmeras dietas, e eu
-          <strong>tenho certeza</strong> que você já <strong>tentou de tudo</strong> para aliviar os
-          sintomas, <strong>mas a verdade é uma só:</strong>
-        </div>
-
-        <div class="text-2xl text-white mb-5 bg-orange-500 dark:bg-orange-400 p-5">
-          NÃO É A DIETA GENERALISTA QUE VAI ALIVIAR OS SINTOMAS DA SII
-        </div>
-
-        <div class="text-2xl mb-10">
-          <strong>Existe um caminho cientificamente comprovado</strong> para controlar os sintomas
-          que você tem… <strong>E ele não se resume</strong> a seguir uma dieta saudável
-        </div>
-
-        <ButtonAgendarConsulta />
-      </div>
-    </section>
-
     <Separator />
 
     <section class="pt-20 pb-20 text-center">
